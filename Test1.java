7ec6383b250d111e736d9bd8cd1eb84d3e4da81c
jchan03 <jeromechan2000@gmail.com>
2014-11-06 13:21:07 -0800

    Ant Test 1

---
+++ Test1.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Test1 {
+
+	@Before
+	public void setUp() throws Exception {
+		assert(1==1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
